5849b58cda84e0db81b4aece9a23d450fe18df63
Gavin <lugavin@outlook.com>
2019-03-10 12:47:40 +0800

    Update

---
+++ cloud-common/cloud-common-base/src/main/java/com/gavin/cloud/common/base/jwt/cipher/RSAVerifier.java
@@ -0,0 +1,34 @@
+package com.gavin.cloud.common.base.jwt.cipher;
+
+import java.security.GeneralSecurityException;
+import java.security.Signature;
+import java.security.interfaces.RSAPublicKey;
+
+public class RSAVerifier implements Verifier {
+
+    private final RSAPublicKey key;
+    private final String alg;
+
+    public RSAVerifier(RSAPublicKey key, String alg) {
+        this.key = key;
+        this.alg = alg;
+    }
+
+    @Override
+    public boolean verify(byte[] data, byte[] sig) {
+        try {
+            Signature signature = Signature.getInstance(alg);
+            signature.initVerify(key);
+            signature.update(data);
+            return signature.verify(sig);
+        } catch (GeneralSecurityException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getAlgorithm() {
+        return alg;
+    }
+
+}
