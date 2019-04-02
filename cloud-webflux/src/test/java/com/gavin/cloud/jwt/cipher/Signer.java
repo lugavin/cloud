aeff98e403ae9fa3708a38deddcb6f433aa40389
Gavin <lugavin@outlook.com>
2019-04-02 21:31:25 +0800

    Update

--- cloud-webflux/src/test/java/com/gavin/cloud/jwt/cipher/Signer.java
+++
@@ -1,7 +0,0 @@
-package com.gavin.cloud.jwt.cipher;
-
-public interface Signer extends Algorithm {
-
-    byte[] sign(byte[] data);
-
-}
--- cloud-webflux/src/test/java/com/gavin/cloud/jwt/cipher/Verifier.java
+++
@@ -1,7 +0,0 @@
-package com.gavin.cloud.jwt.cipher;
-
-public interface Verifier extends Algorithm {
-
-    boolean verify(byte[] data, byte[] sig);
-
-}
