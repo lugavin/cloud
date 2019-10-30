fb0f0e3895d30755cc4f29a59c156126780ac0ed
Gavin <lugavin@outlook.com>
2019-10-30 23:00:31 +0800

    Update

--- cloud-test/src/test/java/com/gavin/cloud/distributed/RedissonTest.java
+++ cloud-test/src/test/java/com/gavin/cloud/distributed/RedissonTest.java
@@ -1,36 +1,59 @@
 package com.gavin.cloud.distributed;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.redisson.Redisson;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
+import org.redisson.api.*;
+import org.redisson.client.codec.StringCodec;
 import org.redisson.config.Config;
 import org.springframework.util.ResourceUtils;
 
 import java.net.URL;
+import java.util.Collections;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+
+import static org.redisson.api.RScript.Mode.READ_WRITE;
+import static org.redisson.api.RScript.ReturnType.BOOLEAN;
 
 @Slf4j
 public class RedissonTest {
+
+    private static final String LUA_SCRIPT =
+            "local currValue = redis.call('get', KEYS[1]); "
+                    + "if tonumber(ARGV[1]) == 0 and currValue == false then "
+                    + "redis.call('set', KEYS[1], ARGV[2]); "
+                    + "return 1 "
+                    + "else "
+                    + "return 0 "
+                    + "end";
 
     private RedissonClient redisson;
 
     @Before
     public void setUp() throws Exception {
-        //URL url = Thread.currentThread().getContextClassLoader().getResource("redisson.yml");
         URL url = ResourceUtils.getURL("classpath:redisson.yml");
-        Config config = Config.fromYAML(url);
-        redisson = Redisson.create(config);
+        redisson = Redisson.create(Config.fromYAML(url));
     }
 
-    @After
-    public void tearDown() {
-        //if (redisson != null) {
-        //    redisson.shutdown();
-        //}
+
+    @Test
+    public void testScript() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        String key = "_COUNTER_";
+        RScript script = redisson.getScript(StringCodec.INSTANCE);
+        RFuture<Boolean> future = script.evalAsync(READ_WRITE, LUA_SCRIPT, BOOLEAN, Collections.singletonList(key), 0, 10);
+        future.thenAccept(ret -> {
+            if (ret) {
+                redisson.getAtomicLong(key).expire(3600, TimeUnit.SECONDS);
+                log.debug("计数器初始化成功");
+            } else {
+                log.debug("计数器已初始化");
+            }
+            latch.countDown();
+        });
+        latch.await();
     }
 
     @Test
@@ -49,7 +72,8 @@ public class RedissonTest {
 
     @Test
     public void testAtomic() {
-        log.debug("====== {} ======", redisson.getAtomicLong("ORDER_NO").incrementAndGet());
+        RAtomicLong rAtomicLong = redisson.getAtomicLong("ORDER_NO");
+        log.debug("====== {} ======", rAtomicLong.incrementAndGet());
     }
 
 }
